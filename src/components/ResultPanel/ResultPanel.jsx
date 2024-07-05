0fee4ccdb14f712f6db88464fe1926e7fdecf0e9
Eugene <jhongil3007@gmail.com>
2024-07-06 00:34:19 +0300

    модальное окно с hover эффектом

--- src/components/ResultPanel/ResultPanel.jsx
+++ src/components/ResultPanel/ResultPanel.jsx
@@ -1,4 +1,5 @@
 import './ResultPanel.css';
+import ResultContent from '../ResultContent/ResultContent';
 
 export default function ResultPanel() {
     return (
@@ -9,29 +10,7 @@ export default function ResultPanel() {
             <div className="image-result">
                 <img src="" alt="Картинка результата поиска" className='image-result__image' />
             </div>
-            <div className="information-result">
-                <h1 className="information-result__title">Тестовая запись</h1>
-                <p className="information-result__author">Тестовый автор</p>
-                <label htmlFor="sponsor" className="information-result__sponsor-checkbox">
-                    <input type="checkbox" name="sponsor" id="sponsor" />
-                    Sponsorblock
-                    <button className="information-result__info">i</button>
-                </label>
-                <div className="information-result__download">
-                    <div className="information-result__download-button-place">
-                        <button type="button" className="information-result__download-button">
-                            Скачать
-                        </button>
-                        <button className="information-result__drop-list">
-                            <img src="/images/arrow.svg" alt="Изображение стрелки выпадающего списка" />
-                        </button>
-                    </div>
-                    <label htmlFor="audio" className="information-result__audio-check">
-                        <input type="checkbox" name="audio" id="audio" className="information-result__checkbox" />
-                        Только звук
-                    </label>
-                </div>
-            </div>
+            <ResultContent />
         </section>
     );
 }
