47e69f35acaa72a91d64e81de718aed304ef9557
Eugene <jhongil3007@gmail.com>
2024-07-05 14:27:05 +0300

    Готовый Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
     return (
         <footer className="footer">
             <a href="#" className="footer__link" target="_blank">
-                <img src="/gitLogo.svg" alt="Логотип GitHub" className="footer__git-logo" />
+                <img src="/images/gitLogo.svg" alt="Логотип GitHub" className="footer__git-logo" />
             </a>
             <a href="#" className="footer__link" target="_blank">
                 <p className="footer__github-text">github</p>
             </a>
-            <img src="/vkLogo.png" alt="Логотип Вконтакте" />
-            <img src="/ytLogo.png" alt="Логотип YouTube" />
+            <img src="/images/vkLogo.png" alt="Логотип Вконтакте" className="footer__vk-logo"/>
+            <img src="/images/ytLogo.png" alt="Логотип YouTube" className='footer__yt-logo'/>
         </footer>
     );
 }
