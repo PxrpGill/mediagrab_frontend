f9915d7690113ce4062365d92cd5730d8c23d963
Eugene <jhongil3007@gmail.com>
2024-07-05 13:30:14 +0300

    Готовый Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="header">
-            <img src="/multiLogo.png" alt="Логотип Multigrab" className="header__logo multi-logo"/>
+            <img src="/images/multiLogo.png" alt="Логотип Multigrab" className="header__logo multi-logo"/>
+            <h1 className="header__title">MultiGrab</h1>
         </header>
     )
 }
