04e65d7a52f0f2719bddd20f46e831ee39e76298
Eugene <jhongil3007@gmail.com>
2024-07-07 14:47:15 +0300

    корректировка стилей и стилизация модального окна

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="header">
-            <img src="/images/multiLogo.png" alt="Логотип Multigrab" className="header__logo multi-logo"/>
+            <img src="/images/multiLogo.png" alt="Логотип Multigrab" className="header__logo multi-logo" draggable="false" />
             <h1 className="header__title">MultiGrab</h1>
         </header>
     )
