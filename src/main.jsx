b0e3204c98a6b41e3f24194ffc8b42252bc2512d
Eugene <jhongil3007@gmail.com>
2024-07-13 17:19:25 +0300

    Fix: Исправлено подключение стилей

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MainPage } from './pages/MainPage'
+import { MainPage } from '@/pages/MainPage'
+import '@/shared/styles/reset_style.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
