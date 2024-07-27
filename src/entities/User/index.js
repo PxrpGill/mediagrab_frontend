df14830fb7fd5613948b530f326aa68699b3849e
Eugene <jhongil3007@gmail.com>
2024-07-27 21:50:03 +0300

    Update: сделана панель для пользователей

---
+++ src/entities/User/index.js
@@ -0,0 +1,1 @@
+export { Cabinet } from './ui/Cabinet';
--- src/widgets/header/ui/Header.jsx
+++ src/widgets/header/ui/Header.jsx
@@ -1,12 +1,18 @@
 import styles from './Header.module.css';
 import multiLogo from '@/shared/assets/images/multiLogo.png';
+import { Cabinet } from '@/entities/User/ui/Cabinet';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src={multiLogo}
-        alt="Логотип Mediagrab" draggable="false" />
-      <h1 className={styles.title}>MediaGrab</h1>
+      <div className={styles.logoPlace}>
+        <img className={styles.logo} src={multiLogo}
+          alt="Логотип Mediagrab" draggable="false" />
+        <h1 className={styles.title}>MediaGrab</h1>
+      </div>
+      <div className={styles.authPlace}>
+        <Cabinet />
+      </div>
     </header>
-  )
-}
+  );
+};
