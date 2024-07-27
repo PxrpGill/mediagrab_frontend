deeb0fdcb86f864051851f3eb8abfe5014e16829
Eugene <jhongil3007@gmail.com>
2024-07-27 22:02:48 +0300

    Update: Создание стора для пользователя

---
+++ src/entities/User/model/index.js
@@ -0,0 +1,30 @@
+import { makeAutoObservable, runInAction } from "mobx";
+
+class User {
+  
+
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  register = async (username, email, password) => {
+    try {
+      const request = new Request("https://mediagrab.ru/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      const response = await fetch(request);
+
+    } catch (error) {
+
+    }
+  }
+
+}
+
+export const user = new User();
