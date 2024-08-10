94b6012758ec5740b3e3e8c2e8c66a4e4b6af9a6
Eugene <jhongil3007@gmail.com>
2024-08-10 17:00:46 +0300

    Update: Написание Mobx User стора

--- src/entities/User/model/index.js
+++ src/entities/User/model/index.js
@@ -1,8 +1,14 @@
 import { makeAutoObservable, runInAction } from "mobx";
 
 class User {
-  
-
+  isRegisterLoading = false;
+  isAuthLoading = false;
+  isAuthError = false;
+  userInformation = {
+    id: null,
+    username: "",
+    email: "",
+  };
 
   constructor() {
     makeAutoObservable(this);
@@ -10,6 +16,9 @@ class User {
 
   register = async (username, email, password) => {
     try {
+      runInAction(() => {
+        this.isRegisterLoading = true;
+      });
       const request = new Request("https://mediagrab.ru/auth/register", {
         method: "POST",
         body: JSON.stringify({
@@ -19,12 +28,52 @@ class User {
         }),
       });
       const response = await fetch(request);
-      
     } catch (error) {
-
+      console.error(error);
+    } finally {
+      runInAction(() => {
+        this.isRegisterLoading = false;
+      });
     }
-  }
+  };
 
+  auth = async (email, password) => {
+    try {
+      runInAction(() => {
+        this.isAuthLoading = true;
+      });
+      const requestTokens = new Request("https://mediagrab.ru/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const responseTokens = await fetch(requestTokens);
+      const dataTokens = await responseTokens.json();
+      if (dataTokens.user_id) {
+        const aboutUserResponse = await fetch(
+          "https://api.mediagrab.ru:228/authorized/about_user"
+        );
+        const dataAbout = await aboutUserResponse.json();
+        runInAction(() => {
+          this.userInformation.id = dataAbout.id;
+          this.userInformation.email = dataAbout.email;
+          this.userInformation.username = dataAbout.username;
+        });
+      } else {
+        runInAction(() => {
+          this.isAuthError = true;
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      runInAction(() => {
+        this.isAuthLoading = false;
+      });
+    }
+  };
 }
 
 export const user = new User();
