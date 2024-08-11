c64596f7c8c83641cfbcab21138acdd6b049c6f2
Eugene <jhongil3007@gmail.com>
2024-08-11 14:01:34 +0300

    Update: About user

--- src/features/userAuth/ui/component.jsx
+++ src/features/userAuth/ui/component.jsx
@@ -3,22 +3,32 @@ import { Cabinet } from '../../../entities/User';
 import { RegisterModal } from './registerModal';
 import { useState } from 'react';
 import { AuthModal } from './authModal';
+import { userStore } from '../../../entities/User';
 
 export const UserAuth = ({ opened, setOpen }) => {
   const [isRegisterOpen, toggleRegister] = useState(false);
   const [isAuthOpen, toggleAuth] = useState(false);
+  const { userInformation } = userStore;
 
   return (
     <Cabinet>
-      <button className={style.button} onClick={() => toggleRegister(true)}>
-        Зарегистрироваться
-      </button>
-      <p className={style.del}>/</p>
-      <button className={style.button} onClick={() => toggleAuth(true)}>
-        Войти
-      </button>
-      <RegisterModal isOpen={isRegisterOpen} toggle={toggleRegister} />
-      <AuthModal isOpen={isAuthOpen} toggle={toggleAuth} />
+      {userInformation.id ? (
+        <>
+          <button className={style.button}>{userInformation.username}</button>
+        </>
+      ) : (
+        <>
+          <button className={style.button} onClick={() => toggleRegister(true)}>
+            Зарегистрироваться
+          </button>
+          <p className={style.del}>/</p>
+          <button className={style.button} onClick={() => toggleAuth(true)}>
+            Войти
+          </button>
+          <RegisterModal isOpen={isRegisterOpen} toggle={toggleRegister} />
+          <AuthModal isOpen={isAuthOpen} toggle={toggleAuth} />
+        </>
+      )}
     </Cabinet>
   );
 };
